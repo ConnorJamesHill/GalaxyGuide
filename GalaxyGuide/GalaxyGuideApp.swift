25e006f6fa7b188fa245a258fb7846372c8b9007
Meruem Moniker <jamesconnorhill@gmail.com>
2024-02-20 19:34:31 -0500

    Initial Commit

---
+++ GalaxyGuide/GalaxyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyGuideApp.swift
+//  GalaxyGuide
+//
+//  Created by Connor Hill on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
